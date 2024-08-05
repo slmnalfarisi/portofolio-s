eaac5a0f2a0fe7e5cf8e645528d749bbdb0d8a42
reynaldo0 <reynaldooyusellino@gmail.com>
2024-08-05 21:30:17 +0700

    fix : qualification data

--- src/Pages/Qualification/Qualification.jsx
+++ src/Pages/Qualification/Qualification.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import "./Qualification.css"
+import React, { useState, useEffect } from "react";
+import "./Qualification.css";
 
 const Qualification = () => {
-  const [activeTab, setActiveTab] = useState('#education');
+  const [activeTab, setActiveTab] = useState("#education");
 
   useEffect(() => {
-    const contents = document.querySelectorAll('.qualification_content');
+    const contents = document.querySelectorAll(".qualification_content");
     contents.forEach((content) => {
       if (content.id === activeTab.slice(1)) {
-        content.style.display = 'block';
+        content.style.display = "block";
         setTimeout(() => {
-          content.classList.add('qualification_active');
+          content.classList.add("qualification_active");
         }, 50);
       } else {
-        content.style.display = 'none';
-        content.classList.remove('qualification_active');
+        content.style.display = "none";
+        content.classList.remove("qualification_active");
       }
     });
   }, [activeTab]);
@@ -30,20 +30,28 @@ const Qualification = () => {
       </h2>
       <span className="section_subtitle"></span>
 
-      <div className="qualification_container container" data-aos="zoom-in" data-aos-duration="3000">
+      <div
+        className="qualification_container container"
+        data-aos="zoom-in"
+        data-aos-duration="3000"
+      >
         <div className="qualification_tabs">
           <div
-            className={`qualification_button button--flex ${activeTab === '#education' ? 'qualification_active' : ''}`}
+            className={`qualification_button button--flex ${
+              activeTab === "#education" ? "qualification_active" : ""
+            }`}
             data-target="#education"
-            onClick={() => handleTabClick('#education')}
+            onClick={() => handleTabClick("#education")}
           >
             <i className="uil uil-graduation-cap qualification_icon"></i>
             Pendidikan
           </div>
           <div
-            className={`qualification_button button--flex ${activeTab === '#work' ? 'qualification_active' : ''}`}
+            className={`qualification_button button--flex ${
+              activeTab === "#work" ? "qualification_active" : ""
+            }`}
             data-target="#work"
-            onClick={() => handleTabClick('#work')}
+            onClick={() => handleTabClick("#work")}
           >
             <i className="uil uil-trophy qualification_icon"></i>
             Kompetisi
@@ -55,7 +63,9 @@ const Qualification = () => {
             <div className="qualification_data">
               <div>
                 <h3 className="qualification_title">Sekolah Dasar</h3>
-                <span className="qualification_subtitle">SDN 02 SUMBERJAYA</span>
+                <span className="qualification_subtitle">
+                  SDN 02 SUMBERJAYA
+                </span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
                   2013-2019
@@ -73,8 +83,12 @@ const Qualification = () => {
                 <span className="qualification_line"></span>
               </div>
               <div>
-                <h3 className="qualification_title">Sekolah Menengah Pertama</h3>
-                <span className="qualification_subtitle">SMPN 11 TAMBUN SELATAN</span>
+                <h3 className="qualification_title">
+                  Sekolah Menengah Pertama
+                </h3>
+                <span className="qualification_subtitle">
+                  SMPN 11 TAMBUN SELATAN
+                </span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
                   2019-2022
@@ -83,7 +97,9 @@ const Qualification = () => {
             </div>
             <div className="qualification_data">
               <div>
-                <h3 className="qualification_title">Sekolah Menengah Kejuruan</h3>
+                <h3 className="qualification_title">
+                  Sekolah Menengah Kejuruan
+                </h3>
                 <span className="qualification_subtitle">SMKN 46 JAKARTA</span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
@@ -99,7 +115,9 @@ const Qualification = () => {
           <div className="qualification_content" data-content id="work">
             <div className="qualification_data">
               <div>
-                <h3 className="qualification_title">Cloud Computing Club Competition ~ Website Statis</h3>
+                <h3 className="qualification_title">
+                  Cloud Computing Club Competition ~ Website Statis
+                </h3>
                 <span className="qualification_subtitle">Peserta</span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
@@ -118,7 +136,9 @@ const Qualification = () => {
                 <span className="qualification_line"></span>
               </div>
               <div>
-                <h3 className="qualification_title">JakBee Competition ~ Short Movie</h3>
+                <h3 className="qualification_title">
+                  JakBee Competition ~ Short Movie
+                </h3>
                 <span className="qualification_subtitle">Grand Finalis</span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
@@ -128,8 +148,12 @@ const Qualification = () => {
             </div>
             <div className="qualification_data">
               <div>
-                <h3 className="qualification_title">Cloud Computing Club Competition ~ Website Statis</h3>
-                <span className="qualification_subtitle">Juara 2 & Best Desain</span>
+                <h3 className="qualification_title">
+                  Cloud Computing Club Competition ~ Website Statis
+                </h3>
+                <span className="qualification_subtitle">
+                  Juara 2 & Best Desain
+                </span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
                   2024
@@ -144,7 +168,11 @@ const Qualification = () => {
               <div></div>
               <div>
                 <span className="qualification_rounder"></span>
-                <h3 className="qualification_title">Lomba Kompetensi Siswa (LKS) ~ Cyber Security</h3>
+              </div>
+              <div>
+                <h3 className="qualification_title">
+                Lomba Kompetensi Siswa (LKS) ~ Cyber Security
+                </h3>
                 <span className="qualification_subtitle">Juara Harapan III</span>
                 <div className="qualification_calendar">
                   <i className="uil uil-calendar-alt"></i>
