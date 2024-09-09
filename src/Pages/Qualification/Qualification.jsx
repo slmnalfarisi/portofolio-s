264b93fe1024c2d864e798c7115c4c554f133e2d
reynaldo0 <reynaldooyusellino@gmail.com>
2024-09-09 07:09:10 +0700

    clean code qualification

--- src/Pages/Qualification/Qualification.jsx
+++ src/Pages/Qualification/Qualification.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Qualification.css";
+import { qualificationData } from "../../docs/QualificationData";
 
 const Qualification = () => {
   const [activeTab, setActiveTab] = useState("#education");
@@ -23,12 +24,52 @@ const Qualification = () => {
     setActiveTab(target);
   };
 
+  const renderQualificationData = (data) => {
+    return data.map((item, index) => (
+      <div className="qualification_data" key={index}>
+        {/* Jika index ganjil, tampilkan data di kiri */}
+        {index % 2 === 0 ? (
+          <>
+            <div>
+              <h3 className="qualification_title">{item.title}</h3>
+              <span className="qualification_subtitle">{item.subtitle}</span>
+              <div className="qualification_calendar">
+                <i className="uil uil-calendar-alt"></i>
+                {item.calendar}
+              </div>
+            </div>
+            <div>
+              <span className="qualification_rounder"></span>
+              {index !== data.length - 1 && <span className="qualification_line"></span>}
+            </div>
+            <div></div> {/* Kosongkan kolom kanan */}
+          </>
+        ) : (
+          <>
+            <div></div> {/* Kosongkan kolom kiri */}
+            <div>
+              <span className="qualification_rounder"></span>
+              {index !== data.length - 1 && <span className="qualification_line"></span>}
+            </div>
+            <div>
+              <h3 className="qualification_title">{item.title}</h3>
+              <span className="qualification_subtitle">{item.subtitle}</span>
+              <div className="qualification_calendar">
+                <i className="uil uil-calendar-alt"></i>
+                {item.calendar}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    ));
+  };
+
   return (
     <section className="qualification section" id="experience">
       <h2 className="section_title" data-aos="zoom-in" data-aos-duration="1400">
         | PENGALAMAN |
       </h2>
-      <span className="section_subtitle"></span>
 
       <div
         className="qualification_container container"
@@ -40,7 +81,6 @@ const Qualification = () => {
             className={`qualification_button button--flex ${
               activeTab === "#education" ? "qualification_active" : ""
             }`}
-            data-target="#education"
             onClick={() => handleTabClick("#education")}
           >
             <i className="uil uil-graduation-cap qualification_icon"></i>
@@ -50,7 +90,6 @@ const Qualification = () => {
             className={`qualification_button button--flex ${
               activeTab === "#work" ? "qualification_active" : ""
             }`}
-            data-target="#work"
             onClick={() => handleTabClick("#work")}
           >
             <i className="uil uil-trophy qualification_icon"></i>
@@ -59,165 +98,12 @@ const Qualification = () => {
         </div>
 
         <div className="qualification_sections">
-          <div className="qualification_content" data-content id="education">
-            <div className="qualification_data">
-              <div>
-                <h3 className="qualification_title">Sekolah Dasar</h3>
-                <span className="qualification_subtitle">
-                  SDN 02 SUMBERJAYA
-                </span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2013-2019
-                </div>
-              </div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div></div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-              <div>
-                <h3 className="qualification_title">
-                  Sekolah Menengah Pertama
-                </h3>
-                <span className="qualification_subtitle">
-                  SMPN 11 TAMBUN SELATAN
-                </span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2019-2022
-                </div>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div>
-                <h3 className="qualification_title">
-                  Sekolah Menengah Kejuruan
-                </h3>
-                <span className="qualification_subtitle">SMKN 46 JAKARTA</span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2022-2025
-                </div>
-              </div>
-              <div>
-                <span className="qualification_rounder"></span>
-              </div>
-            </div>
+          <div className="qualification_content" id="education">
+            {renderQualificationData(qualificationData.education)}
           </div>
 
-          <div className="qualification_content" data-content id="work">
-            <div className="qualification_data">
-              <div>
-                <h3 className="qualification_title">
-                  Cloud Computing Club Competition ~ Website Design
-                </h3>
-                <span className="qualification_subtitle">Peserta</span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2022
-                </div>
-              </div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div></div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-              <div>
-                <h3 className="qualification_title">
-                  JakBee Competition ~ Short Movie
-                </h3>
-                <span className="qualification_subtitle">Grand Finalis</span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2023
-                </div>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div>
-                <h3 className="qualification_title">
-                  Cloud Computing Club Competition ~ Website Design
-                </h3>
-                <span className="qualification_subtitle">
-                  Juara 2 & Best Desain
-                </span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2024
-                </div>
-              </div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div></div>
-              
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-              <div>
-                <h3 className="qualification_title">
-                Lomba Kompetensi Siswa (LKS) ~ Cyber Security
-                </h3>
-                <span className="qualification_subtitle">Juara Harapan III</span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2024
-                </div>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div>
-                <h3 className="qualification_title">
-                  T8NOFEST (Teknologi dan Soundsystem) - Web Design
-                </h3>
-                <span className="qualification_subtitle">
-                  Juara 2 
-                </span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2024
-                </div>
-              </div>
-              <div>
-                <span className="qualification_rounder"></span>
-                <span className="qualification_line"></span>
-              </div>
-            </div>
-            <div className="qualification_data">
-              <div></div>
-              
-              <div>
-                <span className="qualification_rounder"></span>
-                
-              </div>
-              <div>
-                <h3 className="qualification_title">
-                GSS Micro Influencer Competitions - Web Design
-                </h3>
-                <span className="qualification_subtitle">Juara Harapan I</span>
-                <div className="qualification_calendar">
-                  <i className="uil uil-calendar-alt"></i>
-                  2024
-                </div>
-              </div>
-            </div>
+          <div className="qualification_content" id="work">
+            {renderQualificationData(qualificationData.work)}
           </div>
         </div>
       </div>
---
+++ src/docs/QualificationData.js
@@ -0,0 +1,52 @@
+export const qualificationData = {
+    education: [
+      {
+        title: "Sekolah Dasar",
+        subtitle: "SDN 02 SUMBERJAYA",
+        calendar: "2013-2019"
+      },
+      {
+        title: "Sekolah Menengah Pertama",
+        subtitle: "SMPN 11 TAMBUN SELATAN",
+        calendar: "2019-2022"
+      },
+      {
+        title: "Sekolah Menengah Kejuruan",
+        subtitle: "SMKN 46 JAKARTA",
+        calendar: "2022-2025"
+      },
+    ],
+    work: [
+      {
+        title: "Cloud Computing Club Competition ~ Website Design",
+        subtitle: "Peserta",
+        calendar: "2022"
+      },
+      {
+        title: "JakBee Competition ~ Short Movie",
+        subtitle: "Grand Finalis",
+        calendar: "2023"
+      },
+      {
+        title: "Cloud Computing Club Competition ~ Website Design",
+        subtitle: "Juara 2 & Best Desain",
+        calendar: "2024"
+      },
+      {
+        title: "Lomba Kompetensi Siswa (LKS) ~ Cyber Security",
+        subtitle: "Juara Harapan III",
+        calendar: "2024"
+      },
+      {
+        title: "T8NOFEST (Teknologi dan Soundsystem) - Web Design",
+        subtitle: "Juara 2",
+        calendar: "2024"
+      },
+      {
+        title: "GSS Micro Influencer Competitions - Web Design",
+        subtitle: "Juara Harapan I",
+        calendar: "2024"
+      },
+    ]
+  };
+  
